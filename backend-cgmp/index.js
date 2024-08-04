debb96a0c8185f6e1f7d8a02f5810827fd13527e
Zokky2e <icro4.gaming@hotmail.com>
2024-08-04 15:08:39 +0200

    adding mongoose and mongodb to proj

--- backend-cgmp/index.js
+++ backend-cgmp/index.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+	id: String,
+	name: String,
+	age: Number,
+	job: String,
+});
+
+const User = model("user", userSchema);
+mongoose
+	.connect("mongodb://localhost:27017/cgmp-dev", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("Could not connect to MongoDB...", err));
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 
 // Define a simple route
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	User.find()
+		.then((docs) => {
+			console.log(docs);
+			res.send(docs);
+		})
+		.catch((err) => res.send(err));
 });
 
 // Start the server
