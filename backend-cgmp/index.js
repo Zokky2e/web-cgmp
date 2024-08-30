5795a6720f83a078e6f0256d1a63ed47dadd28e2
Zokky2e <icro4.gaming@hotmail.com>
2024-08-30 22:23:52 +0200

    backend change to cors setting

--- backend-cgmp/index.js
+++ backend-cgmp/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
 	cors({
-		origin: "http://localhost:3001", // Replace this with your frontend's origin
+		origin: process.env.FE_URI, // Replace this with your frontend's origin
 		credentials: true,
 	})
 );
